9d2887c70783696e651c3cfa3121fed1987d789b
sasoria <sergio.alberto.arevalo.soria@nav.no>
2022-01-14 17:18:43 +0100

    Bruker samme konvensjon som vite template-en

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Mikrofrontend from "./Mikrofrontend";
-import "./main.css";
+import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
